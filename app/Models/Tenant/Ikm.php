fcc60e1d2d9dd7a323d60408d6f42b1e5a172cb0
= <=>
2023-08-07 10:10:24 +0700

    firts

---
+++ app/Models/Tenant/Ikm.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models\Tenant;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Ikm extends Model
+{
+    use HasFactory;
+    protected $guarded = ['id'];
+}
